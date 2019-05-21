54dba28431559fc42b4532b9d82f743d014a3f66
Cristian Mundo <00086917@uca.edu.sv>
2019-05-21 15:40:38 -0600

    agregando view model, falta el room por eso el error

--- app/src/main/java/com/example/biblioteca/database/viewmodels/BookViewModel.kt
+++ app/src/main/java/com/example/biblioteca/database/viewmodels/BookViewModel.kt
@@ -1,2 +1,42 @@
 package com.example.biblioteca.database.viewmodels
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.biblioteca.database.entities.Author
+import com.example.biblioteca.database.entities.Book
+import com.example.biblioteca.database.entities.Editorial
+import com.example.biblioteca.database.entities.Tag
+import com.example.biblioteca.database.repositories.BookRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class BookViewModel(application: Application) : AndroidViewModel(application){
+
+    private val repository : BookRepository
+    val allBooks : LiveData<List<Book>>
+
+    init {
+        val booksDao = BookRoomDatabase.getDatabase(application).bookDao()
+        repository = BookRepository(booksDao)
+        allBooks = repository.allBooks
+    }
+
+    fun insertAuthor(author: Author) = viewModelScope.launch(Dispatchers.IO){
+        repository.insertAuthor(author)
+    }
+
+    fun insertEditorial(editorial: Editorial) = viewModelScope.launch(Dispatchers.IO){
+        repository.insertEditorial(editorial)
+    }
+
+    fun insertBook(book: Book) = viewModelScope.launch(Dispatchers.IO){
+        repository.insertBook(book)
+    }
+
+    fun insertTag(tag: Tag) = viewModelScope.launch(Dispatchers.IO){
+        repository.insertTag(tag)
+    }
+
+}
