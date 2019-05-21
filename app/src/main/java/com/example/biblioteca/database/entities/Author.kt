2d5a12ee70d403d3a61da264f42318306e96ea65
Cristian Mundo <00086917@uca.edu.sv>
2019-05-21 16:50:24 -0600

    Se han agregado los id a las otras entidades

--- app/src/main/java/com/example/biblioteca/database/entities/Author.kt
+++ app/src/main/java/com/example/biblioteca/database/entities/Author.kt
@@ -1,6 +1,8 @@
 package com.example.biblioteca.database.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "author_table")
 data class Author(var name : String)
+{@PrimaryKey(autoGenerate = true) var id : Long = 0}
