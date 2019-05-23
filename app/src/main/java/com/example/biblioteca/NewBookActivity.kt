08beb326e60796326ec298ecec2f0309c1a1b72a
Cristian Mundo <00086917@uca.edu.sv>
2019-05-23 14:43:02 -0600

    created new book activity

---
+++ app/src/main/java/com/example/biblioteca/NewBookActivity.kt
@@ -0,0 +1,12 @@
+package com.example.biblioteca
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewBookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_book)
+    }
+}
