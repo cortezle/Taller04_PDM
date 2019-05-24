f13461f5f1b15b47b5fe93ab79f8f9668bf1cda2
MaPache07 <00170517@uca.edu.sv>
2019-05-24 02:45:42 -0600

    Se implementaron mas fragmentos

--- app/src/main/java/com/example/biblioteca/BookViewer.kt
+++ app/src/main/java/com/example/biblioteca/BookViewer.kt
@@ -2,37 +2,30 @@ package com.example.biblioteca
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
 import com.example.biblioteca.database.entities.Book
+import com.example.biblioteca.database.viewmodels.BookViewModel
+import com.example.biblioteca.fragments.MainContentFragment
 import com.example.biblioteca.utils.AppConstants
 import kotlinx.android.synthetic.main.activity_book_view.*
 import kotlinx.android.synthetic.main.fragment_main_content.*
 
 
 class BookViewer : AppCompatActivity() {
+
+    private lateinit var mainFragment: MainContentFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_view)
 
-        val receiver : Book = intent?.extras?.getParcelable(AppConstants.TEXT_KEY_BOOK) ?: Book(
-            "N/A",
-            "N/A",
-            1,
-            "N/A",
-            "N/A",
-            0)
-        init(receiver)
+        val receiver : Book = intent?.extras?.getParcelable(AppConstants.TEXT_KEY_BOOK)!!
+        mainFragment = MainContentFragment.newInstance(receiver)
+        changeFragment(R.id.main_cont_fragment, mainFragment)
     }
 
-    fun init(book: Book){
-        Glide.with(this)
-            .load(book.cover)
-            .placeholder(R.drawable.ic_launcher_background)
-            .into(imageView_book_viewer)
-        textView_title_viewer.text = book.title
-        textView_Edition_viewer.text = book.edition.toString()
-        textView_Isbn_viewer.text = book.idBook
-        textView_Synopsis_viewer.text = book.synopsis
+    private fun changeFragment(id: Int, frag: Fragment){ supportFragmentManager.beginTransaction().replace(id, frag).commit() }
 
-    }
 }
--- app/src/main/java/com/example/biblioteca/fragments/MainContentFragment.kt
+++ app/src/main/java/com/example/biblioteca/fragments/MainContentFragment.kt
@@ -5,16 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
 
 import com.example.biblioteca.R
+import com.example.biblioteca.database.entities.Author
 import com.example.biblioteca.database.entities.Book
+import com.example.biblioteca.database.viewmodels.BookViewModel
 import com.example.biblioteca.utils.AppConstants
 import kotlinx.android.synthetic.main.fragment_main_content.view.*
 
 class MainContentFragment : Fragment() {
 
     lateinit var book : Book
+    lateinit var bookViewModel: BookViewModel
 
     companion object{
         fun newInstance (book: Book):MainContentFragment{
@@ -32,10 +36,10 @@ class MainContentFragment : Fragment() {
         super.onCreateView(inflater, container, savedInstanceState)
         val view = inflater.inflate(R.layout.fragment_main_content,container,false)
 
+        bookViewModel = ViewModelProviders.of(this).get(BookViewModel::class.java)
+
         if(savedInstanceState != null) book = savedInstanceState.getParcelable(AppConstants.TEXT_KEY_BOOK)
-
         bindData(view)
-
         return view
     }
 
@@ -47,19 +51,26 @@ class MainContentFragment : Fragment() {
     fun bindData(view: View) {
         view.textView_title.text = book.title
 
-        //view.textView_Author.text = book.authors
+        //view.textView_Author.text = getAuthor(book.idBook)
         view.textView_Edition.text = book.edition.toString()
         //view.textView_Editorial.text = book.editorial
         view.textView_Isbn.text = book.idBook
         view.textView_Synopsis.text = book.synopsis
         //view.textView_Tags.text = book.tags
 
-
         Glide.with(view).load(book.cover)
             .placeholder(R.drawable.ic_launcher_background)
             .into(view.imageView_book)
 
     }
+
+    /*fun getAuthor(bookId : String): String{
+        val array = bookViewModel.getAuthorPerBook(bookId)
+        var cadena = ""
+        /*for(author : Author in array){
+
+        }*/
+    }*/
 
 
 
